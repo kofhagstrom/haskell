b04bb92bfcc45c5191bd229c50a09c4056e7472e
kofhagstrom <69386427+kofhagstrom@users.noreply.github.com>
2022-04-11 20:54:12 +0200

    added comments

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -1,17 +1,35 @@
 type Rule = (Int, String)
 
-makeCycle :: Rule -> [String]
-makeCycle (length, text) = cycle $ replicate (length -1) "" ++ [text]
+-- generate an infinite cycle of list
+cycle' :: [a] -> [a]
+cycle' list = list ++ cycle' list
 
-generalZipWith :: Foldable t => (b -> b -> b) -> t [b] -> [b]
-generalZipWith f = foldl1 (zipWith f)
+-- generate an infinite cycle from a Rule
+-- e.g. (3,"Fizz") -> ["","","Fizz","","","Fizz",...]
+ruleToCycle :: Rule -> [String]
+ruleToCycle (length, text) = cycle' $ replicate (length -1) "" ++ [text]
 
-words' :: [Rule] -> [String]
-words' rules = generalZipWith (++) $ map makeCycle rules
+-- generate an infinite list of words from a list of rules
+-- e.g. [(3,"Fizz"),(5,"Buzz")] -> ["","","Fizz","","Buzz","Fizz",...]
+rulesToWords :: [Rule] -> [String]
+rulesToWords rules = generalZipWith (++) $ map ruleToCycle rules
+  where
+    generalZipWith f = foldl1 (zipWith f)
 
+-- generate an infinite list consisting of generalized fizzbuzz terms from list of Rules
+-- e.g. [(3,"Fizz"),(5,"Buzz")] -> ["1","2","Fizz","4","Buzz","Fizz",...]
 generalFizzBuzz :: [Rule] -> [String]
-generalFizzBuzz rules = zipWith max nums $ words' rules
-    where nums = map show [1 ..]
+generalFizzBuzz rules = zipWith max nums $ rulesToWords rules
+  where
+    nums = map show [1 ..]
 
+-- special case
+-- returns ["1","2","Fizz","4","Buzz","Fizz",...]
 fizzBuzz :: [String]
-fizzBuzz = generalFizzBuzz [(3, "Fizz"), (5,"Buzz")]
+fizzBuzz = generalFizzBuzz [(3, "Fizz"), (5, "Buzz")]
+
+-- special case
+-- returns ["1","2","Fizz","4","Buzz","Fizz","Sazz",8,...]
+fizzBuzzSazz :: [String]
+fizzBuzzSazz = generalFizzBuzz [(3, "Fizz"), (5, "Buzz"), (7, "Sazz")]
+
