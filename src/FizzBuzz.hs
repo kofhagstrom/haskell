05c6c115f9d511e3b67f4d4669493fc61ab1b225
fredrik.f.hagstrom <fredrik.f.hagstrom@afry.com>
2022-04-12 09:16:55 +0200

    new stuff

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -1,27 +1,29 @@
-type Rule = (Int, String)
+module FizzBuzz (generalFizzBuzz, fizzBuzz, fizzBuzzSazz) where
 
--- generate an infinite cycle of list
-cycle' :: [a] -> [a]
-cycle' list = list ++ cycle' list
+type Rule = (Int, String)
 
 -- generate an infinite cycle from a Rule
 -- e.g. (3,"Fizz") -> ["","","Fizz","","","Fizz",...]
-ruleToCycle :: Rule -> [String]
-ruleToCycle (length, text) = cycle' $ replicate (length -1) "" ++ [text]
+cycleFromRule :: Rule -> [String]
+cycleFromRule (length, text) =
+  let cycle' list = list ++ cycle' list
+   in cycle' $ replicate (length -1) "" ++ [text]
 
 -- generate an infinite list of words from a list of rules
 -- e.g. [(3,"Fizz"),(5,"Buzz")] -> ["","","Fizz","","Buzz","Fizz",...]
-rulesToWords :: [Rule] -> [String]
-rulesToWords rules = generalZipWith (++) $ map ruleToCycle rules
-  where
-    generalZipWith f = foldl1 (zipWith f)
+listFromRules :: [Rule] -> [String]
+listFromRules rules =
+  let zipWithMany f = foldl1 (zipWith f)
+      cycles = map cycleFromRule rules
+   in zipWithMany (++) cycles
 
 -- generate an infinite list consisting of generalized fizzbuzz terms from list of Rules
 -- e.g. [(3,"Fizz"),(5,"Buzz")] -> ["1","2","Fizz","4","Buzz","Fizz",...]
 generalFizzBuzz :: [Rule] -> [String]
-generalFizzBuzz rules = zipWith max nums $ rulesToWords rules
-  where
-    nums = map show [1 ..]
+generalFizzBuzz rules =
+  let words = listFromRules rules
+      nums = map show [1 ..]
+   in zipWith max nums words
 
 -- special case
 -- returns ["1","2","Fizz","4","Buzz","Fizz",...]
@@ -29,7 +31,6 @@ fizzBuzz :: [String]
 fizzBuzz = generalFizzBuzz [(3, "Fizz"), (5, "Buzz")]
 
 -- special case
--- returns ["1","2","Fizz","4","Buzz","Fizz","Sazz",8,...]
+-- returns ["1","2","Fizz","4","Buzz","Fizz","Sazz","8",...]
 fizzBuzzSazz :: [String]
 fizzBuzzSazz = generalFizzBuzz [(3, "Fizz"), (5, "Buzz"), (7, "Sazz")]
-
