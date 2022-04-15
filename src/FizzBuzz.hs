31845f961660c38e6a67aa86256b5e0fc1f9bafe
kofhagstrom <69386427+kofhagstrom@users.noreply.github.com>
2022-04-15 19:57:15 +0200

    reformat fizzbuzz

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -1,28 +1,25 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use camelCase" #-}
 module FizzBuzz (generalFizzBuzz, fizzBuzz, fizzBuzzSazz) where
 
 type Rule = (Int, String)
 
--- generate an infinite cycle from a Rule
--- e.g. (3,"Fizz") -> ["","","Fizz","","","Fizz",...]
-cycleFromRule :: Rule -> [String]
-cycleFromRule (length, text) =
-  let cycle' list = list ++ cycle' list
-   in cycle' $ replicate (length -1) "" ++ [text]
+type Cycle = [String]
 
--- generate an infinite list of words from a list of rules
--- e.g. [(3,"Fizz"),(5,"Buzz")] -> ["","","Fizz","","Buzz","Fizz",...]
-listFromRules :: [Rule] -> [String]
-listFromRules rules =
-  let zipWithMany f = foldl1 (zipWith f)
-      cycles = map cycleFromRule rules
-   in zipWithMany (++) cycles
+cycleFromRule :: Rule -> Cycle
+cycleFromRule (length, text) = cycle $ replicate (length -1) "" ++ [text]
 
 -- generate an infinite list consisting of generalized fizzbuzz terms from list of Rules
 -- e.g. [(3,"Fizz"),(5,"Buzz")] -> ["1","2","Fizz","4","Buzz","Fizz",...]
 generalFizzBuzz :: [Rule] -> [String]
 generalFizzBuzz rules =
-  let words = listFromRules rules
-      nums = map show [1 ..]
+  let nums = map show [1 ..]
+      words = listFromRules rules
+        where
+          listFromRules rules =
+            let cycles = map cycleFromRule rules
+             in foldl1 (zipWith (++)) cycles
    in zipWith max nums words
 
 -- special case
@@ -34,3 +31,14 @@ fizzBuzz = generalFizzBuzz [(3, "Fizz"), (5, "Buzz")]
 -- returns ["1","2","Fizz","4","Buzz","Fizz","Sazz","8",...]
 fizzBuzzSazz :: [String]
 fizzBuzzSazz = generalFizzBuzz [(3, "Fizz"), (5, "Buzz"), (7, "Sazz")]
+
+length' :: Num p => [a] -> p
+length' = foldr (\a -> (+) 1) 0
+
+plusMinus :: (Fractional a1, Ord a2, Num a2) => [a2] -> [a1]
+plusMinus arr =
+  let pos = length' $ filter (> 0) arr
+      zero = length' $ filter (== 0) arr
+      neg = length' $ filter (< 0) arr
+      len = length' arr
+   in [pos / len, neg / len, zero / len]
