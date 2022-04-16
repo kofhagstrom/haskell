170c9716583dd63f7d61bea49493cb436abdaa99
kofhagstrom <69386427+kofhagstrom@users.noreply.github.com>
2022-04-16 15:26:20 +0200

    type class stuff

--- src/TimeConversion.hs
+++ src/TimeConversion.hs
@@ -1,15 +1,35 @@
 import Data.Char (digitToInt)
+import Data.List (intercalate, intersperse)
 
-data Time
-  = AM Int Int Int
-  | PM Int Int Int
-  | Military Int Int Int
-  deriving (Show, Eq)
+data Clock = AM | PM | Military deriving (Eq)
+
+instance Show Clock where
+  show Military = ""
+  show AM = "AM"
+  show PM = "PM"
+
+data Time = Time Int Int Int Clock deriving (Eq)
+
+instance Show Time where
+  show (Time hour minute seconds clock) = intercalate ":" [hh, mm, ss] ++ show clock
+    where
+      hh = format hour
+      mm = format minute
+      ss = format seconds
+      format int = (if int < 10 then "0" else "") ++ show int
+
+instance Ord Time where
+  compare t1 t2 = compare (calcTotalTime t1) (calcTotalTime t2)
+
+calcTotalTime :: Time -> Int
+calcTotalTime time = totTime $ convertToMilitary time
+  where
+    totTime (Time hour minute seconds clock) = hour * 3600 + minute * 60 + seconds
 
 findSplits :: Eq b => b -> [b] -> [Int]
 findSplits delimiter = map fst . filter (\x -> snd x == delimiter) . zip [0 ..]
 
-splitTimeString :: [Char] -> ([Char], [Char], [Char], [Char])
+splitTimeString :: String -> (String, String, String, String)
 splitTimeString str = (hour, minute, second, ampm)
   where
     getSplits = splitAt . head $ findSplits ':' str
@@ -28,42 +48,34 @@ parseTime :: String -> Int
 parseTime str = calcTime $ map digitToInt str
 
 getTime :: (String, String, String, String) -> Time
-getTime (h, m, s, ampm) =
-  case length ampm of
-    2 -> case ampm of
-      "PM" -> PM hour minute seconds
-      "AM" -> AM hour minute seconds
+getTime (h, m, s, clock) =
+  case length clock of
+    2 -> case clock of
+      "PM" -> Time hour minute seconds PM
+      "AM" -> Time hour minute seconds AM
       _ -> error "AM or PM."
-    0 -> Military hour minute seconds
+    0 -> Time hour minute seconds Military
     _ -> error "Time string incorrectly formatted."
   where
     hour = parseTime h
     minute = parseTime m
     seconds = parseTime s
 
+stringToTime :: String -> Time
+stringToTime = getTime . splitTimeString
+
 convertToMilitary :: Time -> Time
-convertToMilitary (AM hour minute seconds) =
-  case hour of
-    12 -> Military 0 minute seconds
-    _ -> Military hour minute seconds
-convertToMilitary (PM hour minute seconds) =
-  case hour of
-    12 -> Military 12 minute seconds
-    _ -> Military (12 + hour) minute seconds
-convertToMilitary time = time
-
-parseTimeFromString :: String -> Time
-parseTimeFromString = getTime . splitTimeString
-
-intToStr :: (Ord a, Num a, Show a) => a -> [Char]
-intToStr int = (if int < 10 then "0" else "") ++ show int
-
-timeToString :: Time -> String
-timeToString (PM hour minute seconds) = intToStr hour ++ ":" ++ intToStr minute ++ ":" ++ intToStr seconds ++ "PM"
-timeToString (AM hour minute seconds) = intToStr hour ++ ":" ++ intToStr minute ++ ":" ++ intToStr seconds ++ "AM"
-timeToString (Military hour minute seconds) = intToStr hour ++ ":" ++ intToStr minute ++ ":" ++ intToStr seconds
+convertToMilitary (Time hour minute seconds clock) =
+  case clock of
+    AM -> case hour of
+      12 -> Time 0 minute seconds Military
+      _ -> Time hour minute seconds Military
+    PM -> case hour of
+      12 -> Time 12 minute seconds Military
+      _ -> Time (12 + hour) minute seconds Military
+    Military -> Time hour minute seconds clock
 
 main :: IO ()
 main = do
   time <- getLine
-  putStrLn . timeToString . convertToMilitary $ parseTimeFromString time
+  print . convertToMilitary $ stringToTime time
