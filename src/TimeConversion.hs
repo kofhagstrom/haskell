669dccbb977a6ed9dc0ceef0898f55d2e0d3508c
kofhagstrom <69386427+kofhagstrom@users.noreply.github.com>
2022-04-17 13:14:21 +0200

    formatting

--- src/TimeConversion.hs
+++ src/TimeConversion.hs
@@ -11,67 +11,70 @@ instance Show Clock where
 data Time = Time Int Int Int Clock deriving (Eq)
 
 instance Show Time where
-  show (Time hour minute seconds clock) = intercalate ":" [hh, mm, ss ++ show clock]
+  show (Time hour minute second clock) = intercalate ":" [hh, mm, ss ++ show clock]
     where
       hh = format hour
       mm = format minute
-      ss = format seconds
+      ss = format second
       format int = (if int < 10 then "0" else "") ++ show int
 
 instance Ord Time where
-  compare t1 t2 = compare (calcSeconds t1) (calcSeconds t2)
+  compare t1 t2 = compare (calcsecond t1) (calcsecond t2)
     where
-      calcSeconds time = nSeconds $ convertToMilitary time
-      nSeconds (Time hour minute second _) = zipWith (*) [3600, 60, 1] [hour, minute, second]
-
-findSplits :: Eq b => b -> [b] -> [Int]
-findSplits delimiter = map fst . filter (\x -> snd x == delimiter) . zip [0 ..]
-
-splitTimeString :: String -> (String, String, String, String)
-splitTimeString str = (hour, minute, second, clock)
-  where
-    getSplits = splitAt . head $ findSplits ':' str
-    (hour, a) = getSplits str
-    (minute, b) = getSplits (tail a)
-    c = tail b
-    second = take 2 c
-    clock = drop 2 c
-
-calcTime :: [Int] -> Int
-calcTime time = foldl base10 0 $ zip (reverse time) [0 ..]
-  where
-    base10 = \acc x -> acc + fst x * 10 ^ snd x
-
-getTime :: (String, String, String, String) -> Time
-getTime (hh, mm, ss, clock) =
-  case length clock of
-    2 -> case clock of
-      "PM" -> Time hour minute seconds PM
-      "AM" -> Time hour minute seconds AM
-      _ -> error "AM or PM."
-    0 -> Time hour minute seconds Military
-    _ -> error "Time string incorrectly formatted."
-  where
-    hour = parseTime hh
-    minute = parseTime mm
-    seconds = parseTime ss
-    parseTime = calcTime . map digitToInt
+      calcsecond time = nsecond $ convertToMilitary time
+      nsecond (Time hour minute second _) = zipWith (*) [3600, 60, 1] [hour, minute, second]
 
 stringToTime :: String -> Time
-stringToTime = getTime . splitTimeString
+stringToTime (h1 : h2 : ':' : m1 : m2 : ':' : s1 : s2 : clock) =
+  case length clock of
+    2 -> case clock of
+      "PM" -> Time hour minute second PM
+      "AM" -> Time hour minute second AM
+      _ -> error "AM or PM."
+    0 -> Time hour minute second Military
+    _ -> error "Time string incorrectly formatted."
+  where
+    hour = parseTime [h1, h2]
+    minute = parseTime [m1, m2]
+    second = parseTime [s1, s2]
+    parseTime = calcTime . map digitToInt
+      where
+        calcTime time = foldl base10 0 $ zip (reverse time) [0 ..]
+        base10 = \acc x -> acc + fst x * 10 ^ snd x
+stringToTime _ = error "Time format incorrect"
+
+stringToMaybeTime :: String -> Maybe Time
+stringToMaybeTime (h1 : h2 : ':' : m1 : m2 : ':' : s1 : s2 : clock) =
+  case length clock of
+    2 -> case clock of
+      "PM" -> Just (Time hour minute second PM)
+      "AM" -> Just (Time hour minute second AM)
+      _ -> Nothing
+    0 -> Just (Time hour minute second Military)
+    _ -> Nothing
+  where
+    hour = parseTime [h1, h2]
+    minute = parseTime [m1, m2]
+    second = parseTime [s1, s2]
+    parseTime = calcTime . map digitToInt
+      where
+        calcTime time = foldl base10 0 $ zip (reverse time) [0 ..]
+        base10 = \acc x -> acc + fst x * 10 ^ snd x
+stringToMaybeTime _ = Nothing
+
 
 convertToMilitary :: Time -> Time
-convertToMilitary (Time hour minute seconds clock) =
+convertToMilitary (Time hour minute second clock) =
   case clock of
     AM -> case hour of
-      12 -> Time 0 minute seconds Military
-      _ -> Time hour minute seconds Military
+      12 -> Time 0 minute second Military
+      _ -> Time hour minute second Military
     PM -> case hour of
-      12 -> Time 12 minute seconds Military
-      _ -> Time (12 + hour) minute seconds Military
-    Military -> Time hour minute seconds clock
+      12 -> Time 12 minute second Military
+      _ -> Time (12 + hour) minute second Military
+    Military -> Time hour minute second clock
 
 main :: IO ()
 main = do
-  time <- getLine
-  print . convertToMilitary $ stringToTime time
+  time <- fmap (convertToMilitary . stringToTime) getLine
+  print time
